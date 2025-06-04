df5f2aac3bbb752d8cfa6d8d6336495ebfb3aa08
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-06-04 09:33:50 +0530

    Add files via upload

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MultiDimensionalArray {
+    public static void main(String [] args) {
+         int [][]arr=new int[3][3];
+
+         System.out.println("Row: "+arr.length);
+         System.out.println("Column: "+arr[0].length);
+
+
+    }
+}
