efeec956f827f2cf336c705d3a08e1842a566bb1
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-07-09 15:05:18 +0530

    Add files via upload

---
+++ SumArray.java
@@ -0,0 +1,13 @@
+public interface SumArray {
+    public static int SumArra(){
+        int arr[] =new int[5];
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+           sum +=arr.length[1];
+           System.out.println(sum);
+
+        }
+
+    }
+}
+
